bc67acf4db8778191ac85e021ec6760d56add5ef
rugui chen <rickruguichen@google.com>
2024-10-23 02:56:12 +0000

    update transformers repo path

--- Training/TPU-v5p/Llama2-7B-PyTorch/install_packages.sh
+++ Training/TPU-v5p/Llama2-7B-PyTorch/install_packages.sh
@@ -3,3 +3,4 @@ cd transformers
 pip install .
 pip install torch~=2.4.0 torch_xla[tpu]~=2.4.0 torchvision -f https://storage.googleapis.com/libtpu-releases/index.html
 pip install -U "huggingface_hub[cli]"
+pip install -r requirements
